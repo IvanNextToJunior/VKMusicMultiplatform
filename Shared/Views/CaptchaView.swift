3620473c8f58f8be7c0fbb32d236dea38817654d
IvanNextToJunior <Vanwan@bk.ru>
2022-08-14 11:32:19 +0300

    remove storyboard

---
+++ Shared/Views/CaptchaView.swift
@@ -0,0 +1,20 @@
+//
+//  CaptchaView.swift
+//  VKMisicMultiplatform
+//
+//  Created by Galina on 13.08.2022.
+//
+
+import SwiftUI
+import UIKit
+struct CaptchaView: View {
+    var body: some View {
+ 
+    }
+}
+
+struct CaptchaView_Previews: PreviewProvider {
+    static var previews: some View {
+        CaptchaView()
+    }
+}
