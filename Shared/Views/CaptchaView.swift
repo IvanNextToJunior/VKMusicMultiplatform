0f733d75da73fd7c8169b4973b2ff465d31ca8d0
IvanNextToJunior <Vanwan@bk.ru>
2022-08-16 15:30:31 +0300

    Update CaptchaView.swift

--- Shared/Views/CaptchaView.swift
+++ Shared/Views/CaptchaView.swift
@@ -6,10 +6,47 @@
 //
 
 import SwiftUI
-import UIKit
+import SDWebImageSwiftUI
+import Alamofire
+
 struct CaptchaView: View {
+  
+    @State var captcha_key = ""
+    
+    
     var body: some View {
- 
+        WebImage(url: URL(string: "https://api.vk.com/captcha.php?sid=260120943356")!).scaledToFit()
+            TextField("Введите текст с картинки", text: $captcha_key)
+        Button("Подтвердить") {
+           
+         guard   let urlString = URLQuery.buildURL(baseURL: "https://oauth.vk.com/token", params: ["grant_type" : "password",
+                                                                                        "client_id" : VKClient.officialClient.clientId,
+                                                                                        "client_secret" : VKClient.officialClient.clientSecret,
+                                                                                        "username" : LoginView().login,
+                                                                                        "password" : LoginView().password,
+                                                                                        "v" : "5.116",
+                                                                                        "lang" : "en",
+                                                                                        "scope" : "all"]),
+                 let url = URL(string: urlString) else { return }
+            let headers = HTTPHeaders(["User-Agent" : VKClient.officialClient.userAgent])
+            
+            AF.request(url, headers: headers).responseDecodable(of: AuthorizationResponse.self) { response in
+                switch response.result {
+                case let .success(authResponse):
+                    if let token = authResponse.access_token {
+                        Preferences.shared.accessToken = token
+                        return
+                    }
+
+                case .failure(let err):
+                  debugPrint(err)
+                }
+            
+                    
+        }
+        
+        }
+        
     }
 }
 
